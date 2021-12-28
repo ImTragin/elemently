baad6c6252bff781aa3375fd393b5a483f3c2974
Tragin <tragin@outlook.com>
2021-12-28 17:30:56 +0100

    add in better UX for form

--- src/components/DynamicComponents/tdForm.vue
+++ src/components/DynamicComponents/tdForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form>
+    <form v-if="isFormVisible">
       <v-container>
         <component
           v-for="({ component, title }, index) in fieldItems"
@@ -9,17 +9,47 @@
           :title="title"
           :value="fieldData"
         />
-        <v-btn class="mr-4" @click="submit">
-          submit
-        </v-btn>
+        <v-btn class="mr-4" @click="submit">Submit</v-btn>
+        <v-btn class="mr-4" @click="clearData">Clear</v-btn>
       </v-container>
     </form>
+
+    <div v-if="!isFormVisible">
+      Thank you for your message. I'll get back to your shortly!
+    </div>
+
+    <v-dialog
+      v-model="isSendingDialogVisible"
+      hide-overlay
+      persistent
+      width="300"
+    >
+      <v-card color="primary" dark>
+        <v-card-text>
+          Please stand by
+          <v-progress-linear
+            indeterminate
+            color="#fff"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <v-snackbar v-model="showErrorMessage">
+      {{ errorMessageText }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="#FFC0CB" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
 const axios = require("axios");
-import { required, maxLength, email } from "vuelidate/lib/validators";
 export default {
   beforeMount() {
     var string = "";
@@ -53,19 +83,45 @@ export default {
   },
 
   data: () => ({
+    showErrorMessage: false,
+    errorMessageText: "",
+    isSendingDialogVisible: false,
+    isFormVisible: true,
     fieldData: {},
     fieldItems: [],
   }),
 
   methods: {
     submit() {
+      this.isSendingDialogVisible = true;
       axios
         .post(
           "https://getform.io/f/22ffb686-af9d-43dc-abfb-88c4b4ca9fe7",
           this.fieldData
         )
-        .then(function(response) {
-          console.log(response);
+        .catch((error) => {
+          if (
+            error !== undefined &&
+            error.response !== undefined &&
+            error.response.status === 429
+          ) {
+            this.showErrorMessage = true;
+            this.errorMessageText =
+              "Please wait 60 seconds before sending another message";
+          } else {
+            this.showErrorMessage = true;
+            this.errorMessageText =
+              "We ran into an unknown error when sending your message, please try again.";
+          }
+          this.isSendingDialogVisible = false;
+        })
+        .then((response) => {
+          if (response !== undefined) {
+            if (response.status === 200) {
+              this.isSendingDialogVisible = false;
+              this.isFormVisible = false;
+            }
+          }
         });
     },
 
